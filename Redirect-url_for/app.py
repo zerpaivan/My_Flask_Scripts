1726c40ee58cecbd5963d9fc64d4ca3fead9278e
zerpaian <zerpaivan@gmail.com>
2022-12-17 21:53:05 -0400

    comandos redirect y url_for

---
+++ Redirect-url_for/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/home")
+def index():
+    return "Hello World"
+
+# aca con url_for se retorna la ruta del metodo index
+@app.route("/start")
+def start():
+    return url_for("index")
+
+# redirect permite redireccionar a la url especificada
+@app.route("/google")
+def go_to_google():
+    return redirect("http://www.google.com")
+
+@app.route("/post/<int:id>")
+def post(id):
+    return "Showing post: {}".format(id)
+
+@app.route("/today")
+def today():
+    return redirect(url_for("post", id = 50))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
