b1c1478872fd2ebdf5147c70d635d88b35f9c93d
zerpaian <zerpaivan@gmail.com>
2022-12-25 21:14:08 -0400

    comando request y readme update

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Various scripts to work with flask
 UPLOAD_FILES: Script to upload a file to the server
 
 Redirect-url_for: use of redirect and url_for command samples
+
+request_method: the request method
---
+++ request_method/app.py
@@ -0,0 +1,31 @@
+#  Es un objeto que contiene toda la data enviada desde el cliente (pagina web)
+#  al servidor. La data se obtiene utilizando los metodos POST y GEt.
+# Post se espera que los inputs del usuario sean recibidos por comandos o  una
+# solicitud http
+# Get: La data se recopila directamente sin la necesidad de ser recibida 
+# por comandos o solicitudes http
+
+# Ojetos asociados con request: Form, cookies, Args, Files, Method
+
+# Se importan los modulos necesarios.
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+#  se crea la ruta principal
+@app.route('/')
+def input():
+    return render_template('temp.html')
+
+# otra ruta. 
+# enlace del sitio web
+@app.route('/passing', methods=['GET', 'POST'])
+def display():
+    if request.method == 'POST':
+        result = request.form
+
+        # se envian la data(de result) para resul_data.html
+        return render_template('result_data.html', result=result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
